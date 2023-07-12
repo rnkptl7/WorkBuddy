8af05f0df255e5e635c23f5666a59f3dcde10079
Ronak Patel <ronak.patel@simformsolutions.com>
2023-07-12 13:04:00 +0530

    Integrated firebase

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,28 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { VueFire, VueFireAuth } from 'vuefire'
 
 import App from './App.vue'
 import router from './router'
+import { firebaseApp } from './firebase'
+
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+    components,
+    directives,
+  })
 
 const app = createApp(App)
 
+app.use(VueFire, {firebaseApp})
 app.use(createPinia())
+app.use(vuetify)
 app.use(router)
 
 app.mount('#app')
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,7 +1,76 @@
 <template>
-  <div>Register</div>
+  <div class="registration v-col-8">
+    <form @submit.prevent="registerData">
+      <v-text-field
+        v-model="form.FirstName"
+        :counter="10"
+        label="Firstname"
+        required
+      ></v-text-field>
+
+      <v-text-field
+        v-model="form.LastName"
+        label="Lastname"
+        required
+      ></v-text-field>
+
+      <v-text-field v-model="form.Email" label="E-mail" required></v-text-field>
+
+      <v-text-field
+        v-model="form.Password"
+        label="Password"
+        required
+      ></v-text-field>
+
+      <v-select
+        v-model="form.Department"
+        label="Department"
+        :items="['Frontend', 'Backend', 'HR', 'QA']"
+        required
+      ></v-select>
+
+      <v-select
+        v-model="form.Gender"
+        label="Gender"
+        :items="['Male', 'Female']"
+        required
+      ></v-select>
+
+      <v-select
+        v-model="form.Role"
+        label="Role"
+        :items="['Employee', 'Admin']"
+        required
+      ></v-select>
+
+      <v-date-picker></v-date-picker>
+
+      <v-btn class="me-4" type="submit"> submit </v-btn>
+      <v-btn> clear </v-btn>
+    </form>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, reactive } from "vue";
+import { useFirestore } from "vuefire";
+import { getDocs, collection, addDoc } from "firebase/firestore";
 
-<style lang="scss" scoped></style>
+const db = useFirestore();
+
+const form = reactive({
+  FirstName: "",
+  LastName: "",
+  Email: "",
+  Password: "",
+  Department: "",
+  Gender: "",
+  Role: "",
+});
+
+const registerData = () => {
+  addDoc(collection(db, "users"), form);
+};
+</script>
+
+<style scoped></style>
