87c99296fa3b87e487d26e5b9a7d1fb942a0bc3c
ShyamModha-Simform <shyam.m@simformsolutions.com>
2023-07-13 19:39:55 +0530

    Added: Leave Request Cards, Form Modal, History Table

---
+++ src/components/ApplyLeaveModal.vue
@@ -0,0 +1,180 @@
+<template>
+    <v-row justify="end">
+        <v-dialog v-model="props.dialog" persistent width="500">
+            <!-- <template v-slot:activator="{ props }">
+                <v-btn
+                    variant="outlined"
+                    color="grey-darken-3"
+                    class="btn my-3"
+                    v-bind="props"
+                >
+                    <span id="plus">+ </span> Request For Leave
+                </v-btn>
+            </template> -->
+            <v-card>
+                <div class="pa-5">
+                    <h2>Request for Leave</h2>
+                    <v-divider class="py-1"></v-divider>
+                    <VForm class="form">
+                        <div class="inputDiv">
+                            <select name="category" class="input">
+                                <option value="" disabled selected>
+                                    Leave Category*
+                                </option>
+                                <option value="Hardware">Unplanned</option>
+                                <option value="Software">Planned</option>
+                            </select>
+                            <ErrorMessage
+                                name="category"
+                                class="error_message"
+                            />
+                        </div>
+
+                        <div class="inputDiv">
+                            <input
+                                name="title"
+                                placeholder="Leave message*"
+                                type="text"
+                                class="input"
+                            />
+                            <ErrorMessage name="title" class="error_message" />
+                        </div>
+
+                        <div
+                            class="inputDiv flex-column justify-space-around w-100"
+                        >
+                            <div class="date_input_wrapper w-50">
+                                <label for="startDate ">Start Date</label>
+                                <input
+                                    name="startDate"
+                                    placeholder="Leave message*"
+                                    type="date"
+                                    class="input w-100"
+                                />
+                                <ErrorMessage
+                                    name="title"
+                                    class="error_message"
+                                />
+                            </div>
+                            <div class="date_input_wrapper w-50">
+                                <label for="endDate">End Date</label>
+                                <input
+                                    name="startDate"
+                                    placeholder=""
+                                    type="date"
+                                    class="input w-100"
+                                />
+                                <ErrorMessage
+                                    name="title"
+                                    class="error_message"
+                                />
+                            </div>
+                        </div>
+
+                        <div class="inputDiv">
+                            <textarea
+                                name="description"
+                                placeholder="Description*"
+                                type="text"
+                                class="input"
+                            />
+                            <ErrorMessage
+                                name="description"
+                                class="error_message"
+                            />
+                        </div>
+
+                        <div class="inputDiv">
+                            <input
+                                name="title"
+                                placeholder="Requesting To*"
+                                type="email"
+                                class="input"
+                            />
+                            <ErrorMessage name="title" class="error_message" />
+                        </div>
+
+                        <small>*indicates required field</small>
+
+                        <v-spacer></v-spacer>
+                        <div class="d-flex justify-end">
+                            <v-btn type="reset" @click="closeModal"
+                                >Cancel</v-btn
+                            >
+                            <v-btn
+                                class="me-4 text-white ml-2 btn-submit"
+                                type="submit"
+                                color="#115173"
+                                @click.prevent="closeModal"
+                            >
+                                Submit
+                            </v-btn>
+                        </div>
+                    </VForm>
+                </div>
+            </v-card>
+        </v-dialog>
+    </v-row>
+</template>
+
+<script setup lang="ts">
+    import { ref } from "vue";
+    const dialog = ref(false);
+    const props = defineProps(["dialog"]);
+    const emits = defineEmits(["closeLeaveRequestModal"]);
+
+    function closeModal() {
+        emits("closeLeaveRequestModal", false);
+    }
+</script>
+
+<style scoped>
+    .btn {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    #plus {
+        font-size: 2rem;
+        font-weight: 300;
+        margin-top: -4px;
+        padding-right: 2px;
+    }
+
+    .form-heading {
+        font-weight: 900;
+    }
+
+    .form-heading::after {
+        content: "";
+        width: 50%;
+        height: 2px;
+        background: #000000;
+        display: block;
+        margin: auto;
+    }
+
+    .form .inputDiv {
+        margin: 15px 0;
+        display: flex;
+    }
+
+    .form .inputDiv input,
+    textarea,
+    .inputDiv select {
+        padding: 10px;
+        display: block;
+        width: 100%;
+        outline: none;
+        border: 1px solid #0000003d;
+    }
+
+    .form .inputDiv .date_input_wrapper {
+        width: 40%;
+    }
+
+    .error_message {
+        color: #f44b4b;
+    }
+</style>
---
+++ src/components/LeaveRequestCard.vue
@@ -0,0 +1,65 @@
+<template>
+    <div
+        class="leave-detail_card d-flex flex-fill align-center border rounded-lg my-2"
+    >
+        <div class="ld-card_logo ma-2 d-flex flex-column align-center">
+            <h3>{{ logoDateString.date }} {{ logoDateString.month }}</h3>
+            <h1>{{ logoDateString.year }}</h1>
+        </div>
+        <div class="ld-card-body d-flex flex-column ma-2">
+            <h3>Request Message</h3>
+            <div class="ld-category d-flex flex-row">
+                <span
+                    class="border rounded-pill px-2 text-caption text-uppercase"
+                    >Unplanned</span
+                >
+            </div>
+            <div class="ld-card-body_description">
+                <p>{{ trmimedDescrition }}</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+    import moment from "moment";
+    import { computed, ref } from "vue";
+
+    const date = moment(new Date()).format("Do MMM YYYY").split(" ");
+
+    let description =
+        ref(`Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Repudiandae distinctio voluptates velit. Blanditiis
+                    assumenda dignissimos ad molestias, adipisci eum veniam
+                    ullam accusantium ut! In, pariatur distinctio debitis
+                    repellat commodi voluptas necessitatibus a. Voluptates ipsa
+                    vitae veniam`);
+    const logoDateString = computed(() => {
+        return {
+            date: `${date[0]}`,
+            month: `${date[1]}`,
+            year: `${date[2]}`,
+        };
+    });
+    const year = `${date[2]}`;
+    const trmimedDescrition = computed(() => {
+        return description.value?.slice(0, 50) + "...";
+    });
+</script>
+
+<style scoped>
+    .leave-detail_card {
+        background-color: #f0f3fb;
+        min-width: 300px;
+        height: fit-content;
+    }
+    .ld-card-body_description {
+    }
+    .ld-category > span {
+        background-color: rgb(255, 240, 220);
+        color: orangered;
+    }
+    .ld-card_logo > h3 {
+        width: 70px;
+    }
+</style>
