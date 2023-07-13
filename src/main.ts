43094b69a4adaf3e05ca9b06744c535ea0b7cc98
ShyamModha-Simform <shyam.m@simformsolutions.com>
2023-07-13 19:42:20 +0530

    Added: font awasome library for vuetify icons

--- src/main.ts
+++ src/main.ts
@@ -14,12 +14,25 @@ import "v-calendar/style.css";
 
 // Vuetify
 import "vuetify/styles";
+import "@fortawesome/fontawesome-free/css/all.css";
 import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
+import { VDataTable } from "vuetify/labs/VDataTable";
+import { aliases, fa } from "vuetify/iconsets/fa";
 
 const vuetify = createVuetify({
-    components,
+    icons: {
+        defaultSet: "fa",
+        aliases,
+        sets: {
+            fa,
+        },
+    },
+    components: {
+        ...components,
+        VDataTable,
+    },
     directives,
 });
 
